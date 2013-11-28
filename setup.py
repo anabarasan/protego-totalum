1227614f5df8ddc9130862a1146aa338280e19d6
Anbarasan <nasarabna@gmail.com>
2013-11-28 17:52:18 +0530

    included custom domain for adminstrator

--- setup.py
+++ setup.py
@@ -8,15 +8,11 @@ from app.util import renderTemplate
 
 class setupAuthApp(webapp2.RequestHandler):
     def get(self):
-        script = """
-        <script>
-            var setup = function(){
-                form = document.createElement('form');
-                form.method='POST';
-                form.action = '/setup';
-                form.submit();
-            }
-        </script>
+        form = """
+            <form method='post' action='/setup'>
+                administrator@<input type="text" placeholder="domain.com" name="domain" id="domain" style="width:250px;"/><br/>
+                <input type="Submit" value="Setup" style="width:250px;"/>
+            </form>
         """
         
         cntr = 0
@@ -27,20 +23,22 @@ class setupAuthApp(webapp2.RequestHandler):
             break
         
         if cntr > 0:
-            self.response.out.write(renderTemplate("setup.htm", {'script' : '', 'message' : 'Application Setup has already been completed'}))
+            self.response.out.write(renderTemplate("setup.htm", {'message' : 'Application Setup has already been completed'}))
         else:
-            self.response.out.write(renderTemplate("setup.htm", {'script' : script, 'message' : '<input type="button" value="Setup" onClick="javascript:setup();" style="width:250px;"/>'}))
+            self.response.out.write(renderTemplate("setup.htm", {'message' : form}))
             
     def post(self):
         user = {}
-        
+        domain = self.request.get('domain')
+        if (domain.strip() == '') or (domain == None):
+            domain = "protegototalum.com"
         user['firstName'] = 'Protego Totalum'
         user['lastName'] = 'Administrator'
-        user['email'] = 'administrator@protegototalum.com'
+        user['email'] = 'administrator@%s' % (domain)
         user['password'] = 'password'
 
         identity('setup').create(user)
         
-        self.response.out.write(renderTemplate("setup.htm", {'script' : '', 'message' : '<span class = ".title">Application setup has been completed successfully. <br/>Please make a note of the admin user name and password. <br/> UserId : %s <br/> Password : %s</span>' % (user['email'], user['password'])}))
+        self.response.out.write(renderTemplate("setup.htm", {'message' : '<span class = ".title">Application setup has been completed successfully. <br/>Please make a note of the admin user name and password. <br/> UserId : %s <br/> Password : %s</span>' % (user['email'], user['password'])}))
         
 app = webapp2.WSGIApplication([('/setup', setupAuthApp)], debug=True)
